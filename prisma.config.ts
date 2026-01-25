bdf346933b06eb06d8da6a6d069d73a115077833
euarjunio <aroldojunio16@gmail.com>
2026-01-25 02:03:17 -0300

    aa

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env('DATABASE_URL'),
+    url: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_aXnZRm8Oe0YQ@ep-super-resonance-acbw5n3b-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
   },
 })
