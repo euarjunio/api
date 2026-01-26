2a30228deac217619430277a91efaaf3b0df2d86
euarjunio <aroldojunio16@gmail.com>
2026-01-26 17:22:34 -0300

    prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,5 @@
 import 'dotenv/config'
 import { defineConfig } from 'prisma/config'
-import { env } from './src/config/env.ts'
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
@@ -8,6 +7,6 @@ export default defineConfig({
     path: 'prisma/migrations',
   },
   datasource: {
-    url: env.DATABASE_URL || 'postgresql://neondb_owner:npg_aXnZRm8Oe0YQ@ep-super-resonance-acbw5n3b-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+    url: "postgresql://neondb_owner:npg_aXnZRm8Oe0YQ@ep-super-resonance-acbw5n3b-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
   },
 })
